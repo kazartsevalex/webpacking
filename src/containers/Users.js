006d8bcafbc21ebc01a4e0c6bbaa8d85d0df40b8
Alex K <damned182@mail.ru>
2019-10-09 20:35:44 +0300

    basic react app

---
+++ src/containers/Users.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import PizzaImage from '../components/PizzaImage/PizzaImage';
+
+class Users extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>The Users</h1>
+        <p>Awesome Users container!</p>
+      </div>
+    );
+  }
+}
+
+export default Users;
